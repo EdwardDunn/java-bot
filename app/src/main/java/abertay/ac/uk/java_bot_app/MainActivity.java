b1050e52592d67e7f9c605de5a755d2a1e8e424d
Edward1505629 <1505629@uad.ac.uk>
2018-02-22 11:35:21 +0000

    Added functionality for adding new text view on button click

--- app/src/main/java/abertay/ac/uk/java_bot_app/MainActivity.java
+++ app/src/main/java/abertay/ac/uk/java_bot_app/MainActivity.java
@@ -1,13 +1,65 @@
 package abertay.ac.uk.java_bot_app;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup.LayoutParams;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private LinearLayout layout;
+    private Button ask_btn;
+    private EditText question_field;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupUIViews();
+
+        ask_btn.setOnClickListener(this);
+    }
+
+    private void setupUIViews(){
+        layout = findViewById(R.id.main_ll_question_layout);
+        ask_btn = findViewById(R.id.main_btn_ask);
+        question_field = findViewById(R.id.main_et_question_field);
+    }
+
+    public void onPause(){
+        super.onPause();
+    }
+
+    public void onResume(){
+        super.onResume();
+    }
+
+    @Override
+    public void onClick(View view){
+        if(view.getId() == R.id.main_btn_ask){
+            String question = "";
+            question  = question_field.getText().toString();
+            layout.addView(createNewTextView(question));
+
+        }else{
+
+        }
+    }
+
+    private TextView createNewTextView(String text){
+
+        final LayoutParams lparams = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        final TextView textView = new TextView(this);
+        textView.setLayoutParams(lparams);
+        textView.setText("New text: " + text);
+        return textView;
+
     }
 }
