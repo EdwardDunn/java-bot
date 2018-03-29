2deabad8318af73faa5e0a4fd9d6d2238b7ba71c
Edward1505629 <1505629@uad.ac.uk>
2018-03-29 16:02:26 +0100

    Added dialog box theme

--- app/src/main/java/abertay/ac/uk/java_bot_app/SetupActivity.java
+++ app/src/main/java/abertay/ac/uk/java_bot_app/SetupActivity.java
@@ -4,16 +4,18 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ContextThemeWrapper;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Switch;
-import android.widget.Toast;
 
 public class SetupActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Switch notificationsSwitch;
     //Used to delete data from questions SQLite database
     private Button clearDataBtn;
+
+    private QuestionsSQLiteDatabaseHelper questionsDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +26,8 @@ public class SetupActivity extends AppCompatActivity implements View.OnClickList
 
         notificationsSwitch.setOnClickListener(this);
         clearDataBtn.setOnClickListener(this);
+
+        questionsDatabase = new QuestionsSQLiteDatabaseHelper(this);
 
     }
 
@@ -44,27 +48,28 @@ public class SetupActivity extends AppCompatActivity implements View.OnClickList
             setNotificationsSwitch();
         }
         else if(view.getId() == R.id.setup_btn_clear_data){
-            removeTrainingSessionData();
+
             openDialog(view);
         }
     }
 
     public void openDialog(View view){
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-        alertDialogBuilder.setMessage("Are you sure you want to do this");
-
-        alertDialogBuilder.setPositiveButton("yes",
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(new ContextThemeWrapper(this, R.style.AlertDialogCustom));
+        alertDialogBuilder.setMessage(R.string.remove_training_data_message)
+                            .setTitle(R.string.training_data_title);
+        alertDialogBuilder.setIcon(R.drawable.icon_warning);
+        
+        alertDialogBuilder.setPositiveButton(R.string.alert_dialog_positive,
                 new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-                        Toast.makeText(SetupActivity.this, "you clicked yes", Toast.LENGTH_SHORT).show();
+                        removeTrainingSessionData();
                     }
                 });
 
-        alertDialogBuilder.setNegativeButton("no", new DialogInterface.OnClickListener() {
+        alertDialogBuilder.setNegativeButton(R.string.alert_dialog_negative, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                Toast.makeText(SetupActivity.this, "you clicked no", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -78,9 +83,15 @@ public class SetupActivity extends AppCompatActivity implements View.OnClickList
     }
 
     private void removeTrainingSessionData(){
-        QuestionsSQLiteDatabaseHelper questionsDatabase = new QuestionsSQLiteDatabaseHelper(this);
-        // Delete data from SQLite database
-        questionsDatabase.emptyDatabase();
-        Toast.makeText(this, "Training session data removed", Toast.LENGTH_SHORT).show();
+        Runnable emptyDbRunnable = new Runnable() {
+            @Override
+            public void run() {
+                // Delete data from SQLite database
+                questionsDatabase.emptyDatabase();
+            }
+        };
+        Thread emptyDbThread = new Thread(emptyDbRunnable);
+        emptyDbThread.start();
+
     }
 }
